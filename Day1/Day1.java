bf836dc73eeba83bf062ca33db78b30652abe054
Arin Santry <arinsantry@gmail.com>
2022-01-07 19:20:14 -0500

    added folders

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -1,15 +1,16 @@
-// 697 - too low
+package Day1;
+// How many measurements are larger than the previous measurement?
 
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-public class Day1_2 {
+
+public class Day1
+{
     public static void main(String[] args) {
-        int sum1 = 0;
-        int sum2 = 0;
-        int nums[] = new int[2000];
-        int sums[] = new int[2000];
+        int prev = 9999;
+        int curr;
         int lineNum = 0;
         int numIncrease = 0;
         String lineString;
@@ -18,31 +19,23 @@ public class Day1_2 {
           File infile = new File("inputDay1.txt");
           Scanner myReader = new Scanner(infile);
           while (myReader.hasNextLine()) {
+            lineNum++;
             lineString = myReader.nextLine();
             lineInt = Integer.parseInt(lineString);
-
-            nums[lineNum] = lineInt;
-            lineNum++;
+            curr = lineInt;
+            if(lineNum > 1)
+            {
+                if(isLarger(prev, curr))
+                {
+                    numIncrease++;
+                }
+            }
+            prev = lineInt;
           }
           myReader.close();
         } catch (FileNotFoundException e) {
           System.out.println("An error occurred.");
           e.printStackTrace();
-        }
-
-        for (int i = 0; i < nums.length - 2; i++)
-        {
-            sums[i] = nums[i] + nums[i+1] + nums[i+2];
-        }
-
-        for (int i = 0; i < sums.length - 1; i++)
-        {
-            sum1 = sums[i];
-            sum2 = sums[i+1];
-            if(isLarger(sum1, sum2))
-            {
-                numIncrease++;
-            }
         }
 
         System.out.println(numIncrease);
@@ -55,4 +48,7 @@ public class Day1_2 {
             else
                 return false;
       }
+
+
+
 }
